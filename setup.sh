c41070af3d0f85c5a8be911700e0d8eb0efc39c9
Neal Hardesty <nealhardesty@yahoo.com>
2015-09-01 11:30:13 -0700

    simple setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+GITREPO=$(cd $(dirname $0); pwd)
+
+find . -depth 1 \! -name '.git' \! -name '.ssh' -name '.*' -exec ln -sfv $GITREPO{} ~ \;
+
+mkdir -p ~/.ssh
+ln -sfv $GITREPO/.ssh/config ~/.ssh/config
+
+if [ $(uname) = "Darwin" ]; then
+	ln -sfv ~/.tmux.conf.mac ~/.tmux.conf
+else
+	ln -sfv ~/.tmux.conf.linux ~/.tmux.conf
+fi
