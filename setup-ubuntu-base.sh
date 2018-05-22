ce314b08d10530a372caabc12aec43921a41ac50
Neal Hardesty <nealhardesty@yahoo.com>
2018-05-22 20:53:09 +0000

    added pip

--- setup-ubuntu-base.sh
+++ setup-ubuntu-base.sh
@@ -2,6 +2,6 @@
 
 sudo apt update
 sudo apt remove -y nano
-sudo apt install -y default-jdk build-essential python3 tmux git openssh-server vim samba cifs-utils wget curl jq net-tools dnsutils nmap htop nmon
+sudo apt install -y default-jdk build-essential python3 tmux git openssh-server vim samba cifs-utils wget curl jq net-tools dnsutils nmap htop nmon python3-pip
 
 setxkbmap -option caps:swapescape
