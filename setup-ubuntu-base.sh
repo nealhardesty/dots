57b092951d09afa9aa4c6283f011156dad37fb81
Neal Hardesty <nealhardesty@yahoo.com>
2018-12-30 12:14:52 -0800

    wip

--- setup-ubuntu-base.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-sudo apt update
-sudo apt remove -y nano
-sudo apt install -y default-jdk build-essential python3 tmux git openssh-server vim samba cifs-utils wget curl jq net-tools dnsutils nmap htop nmon python3-pip rsync
-
-setxkbmap -option caps:swapescape
