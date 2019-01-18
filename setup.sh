016ce1b9ad0da56b4871ca62ebb0ff984245bd32
Neal Hardesty <nealhardesty@yahoo.com>
2019-01-18 14:22:41 -0800

    Actually install hammerspoon dir on a mac

--- setup.sh
+++ setup.sh
@@ -7,12 +7,17 @@ find $GITREPO -maxdepth 1 \! -name .gitignore -type f -name '.*' -exec ln -sfv {
 
 rm -rf ~/.vim
 ln -sfv $GITREPO/.vim ~/.vim
-(mkdir -p $GITREPO/.vim/bundle && cd $GITREPO/.vim/bundle/ && rm -rf nerdtree && git clone https://github.com/scrooloose/nerdtree)
+#(mkdir -p $GITREPO/.vim/bundle && cd $GITREPO/.vim/bundle/ && rm -rf nerdtree && git clone https://github.com/scrooloose/nerdtree)
 
 rm -rf ~/.i3
 ln -sfv $GITREPO/.i3 ~/.i3
 
 touch ~/.bashrc.local
+
+if [ $(uname) == "Darwin" ]; then
+  echo Must be a mac, installing hammerspoon config
+  ln -sfv $GITREPO/.hammerspoon ~/.hammerspoon
+fi
 
 
 mkdir -p ~/.ssh
