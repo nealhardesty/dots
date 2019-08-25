53742f767faa135cb299b0a86b1e0ba4b9237789
Neal Hardesty <neal@crunchbase.com>
2019-08-25 11:23:45 -0700

    Install vim, go and python in vscode

--- install-ubuntu-vscode.sh
+++ install-ubuntu-vscode.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-cd ~/Downloads
+#cd ~/Downloads
 
-curl -L -o vscode.deb https://go.microsoft.com/fwlink/?LinkID=760868
+#curl -L -o vscode.deb https://go.microsoft.com/fwlink/?LinkID=760868
 
-sudo dpkg -i vscode.deb
-sudo apt --fix-broken -y install
+#sudo dpkg -i vscode.deb
+#sudo apt --fix-broken -y install
+
+sudo snap install --classic vscode
+/snap/bin/code --install-extension vscodevim.vim
+/snap/bin/code --install-extension ms-python.python
+/snap/bin/code --install-extension ms-vscode.Go
