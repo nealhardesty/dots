5c2bdc21c022ee375585c28231862e452608347f
Neal Hardesty <neal@crunchbase.com>
2019-10-11 09:58:00 -0700

    More macos

--- install-macos-dev.sh
+++ install-macos-dev.sh
@@ -2,9 +2,11 @@
 
 
 
-echo Installing Homebrew
+echo Installing Homebrew...
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install tmux yq wget unrar socat python3 openssl go coreutils autossh gor htop nmap
 
-echo Macdown markdown editor...
-brew cask install macdown
+brew cask install macdown hammerspoon docker dropbox
+
+
