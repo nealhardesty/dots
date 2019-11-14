7460db2ed3d154b847eaf873f4c3e8d9088e953c
Neal Hardesty <neal@crunchbase.com>
2019-11-13 16:19:31 -0800

    More themes

--- install-ubuntu-themes.sh
+++ install-ubuntu-themes.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository -y -n ppa:noobslab/themes
 sudo add-apt-repository -y -n ppa:noobslab/icons
 sudo add-apt-repository -y -n ppa:tista/adapta
 sudo apt update -y
-sudo apt install -y pop-theme gnome-tweaks gnome-shell-extensions arc-theme arc-icons adapta-gtk-theme flat-remix-themes
+sudo apt install -y pop-theme gnome-tweaks gnome-shell-extensions arc-theme arc-icons adapta-gtk-theme flat-remix-themes flat-remix-icons
 # see extensions.gnome.org
 
 
