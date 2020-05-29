f9e8619edf5e9a82b6b4e3d3680c874d2a9c30a0
Neal Hardesty <nealhardesty@yahoo.com>
2020-05-29 12:25:34 -0700

    Windows

---
+++ install-windows-dev.ps1
@@ -0,0 +1,35 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install -y firefox 
+choco install -y sumatrapdf 
+choco install -y 7zip 
+choco install -y sharpkeys
+choco install -y paint.net 
+choco install -y irfanview 
+choco install -y inkscape 
+choco install -y visualstudiocode 
+choco install -y vlc 
+choco install -y chrome 
+choco install -y battle.net 
+#choco install -y spotify 
+#choco install -y dropbox
+choco install -y microsoft-windows-terminal
+
+
+# swap capslock to escape
+Invoke-Expression "cmd.exe /C start capsToEscape.reg"
+
+
+
+# https://docs.microsoft.com/en-us/windows/wsl/install-win10
+
+# windows subsystem
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
+# wsl 2
+#dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+#wsl --set-default-version 2
+
+Invoke-Expression "cmd.exe /C start https://aka.ms/wslstore"
