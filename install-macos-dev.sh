d8fd9ece5ce424277fa1cdf499e783a98d4dd2ad
Neal Hardesty <neal@crunchbase.com>
2020-07-12 14:12:24 -0700

    updated mac

--- install-macos-dev.sh
+++ install-macos-dev.sh
@@ -7,6 +7,8 @@ echo Installing Homebrew...
 
 brew install tmux yq wget unrar socat python3 openssl go coreutils autossh gor htop nmap
 
-brew cask install macdown hammerspoon docker dropbox
+brew cask install iterm2 hammerspoon docker dropbox visual-studio-code firefox brave-browser 1password 1password-cli jetbrains-toolbox vlc evernote adoptopenjdk xquartz slack skype zoomus github kindle viscosity
+
+#brew cask install wine-stable wireshark inkscape macdown joplin aerial vmware-fusion parallels datagrip veracrypt spotify jump
 
 
