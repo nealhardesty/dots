ab5e9162090b7e15a37ac7eaf74a598b09758c06
Neal Hardesty <neal@crunchbase.com>
2020-07-19 10:27:56 -0700

    themes

---
+++ install-ubuntu-desktops.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+
+sudo apt install -y cinnamon xfce4
--- install-ubuntu-themes.sh
+++ install-ubuntu-themes.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-sudo add-apt-repository -y -n ppa:system76/pop
-sudo add-apt-repository -y -n ppa:noobslab/themes
-sudo add-apt-repository -y -n ppa:noobslab/icons
-sudo add-apt-repository -y -n ppa:tista/adapta
-sudo apt update -y
-sudo apt install -y pop-theme gnome-tweaks gnome-shell-extensions arc-theme arc-icons adapta-gtk-theme flat-remix-themes flat-remix-icons
+#sudo add-apt-repository -y -n ppa:noobslab/themes
+#sudo add-apt-repository -y -n ppa:noobslab/icons
+#sudo add-apt-repository -y -n ppa:tista/adapta
+#sudo apt install -y pop-theme gnome-tweaks gnome-shell-extensions arc-theme arc-icons adapta-gtk-theme flat-remix-themes flat-remix-icons
 # see extensions.gnome.org
+
+sudo add-apt-repository -y -n ppa:system76/pop
+sudo apt update -y
+
+sudo apt install -y git gnome-tweaks gnome-shell-extensions pop-theme
+
+git clone https://github.com/rtlewis88/rtl88-Themes.git ~/.themes && rm -rvf ~/.themes/.git ~/.themes/*.jpg ~/.themes/*.md
 
 
