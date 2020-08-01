17a606499406ff94f54e169400eaaa8a0cfef490
Neal Hardesty <neal@crunchbase.com>
2020-08-01 09:04:06 -0700

    joplin

---
+++ install-ubuntu-joplin.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash -x
--- install-ubuntu-kubeadm.sh
+++ install-ubuntu-kubeadm.sh
@@ -14,4 +14,4 @@ sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
 
-sudo curl -Lo /usr/local/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.8.1/kind-linux-amd64 && sudo chmod a+x /usr/local/bin/kind
+#sudo curl -Lo /usr/local/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.8.1/kind-linux-amd64 && sudo chmod a+x /usr/local/bin/kind
