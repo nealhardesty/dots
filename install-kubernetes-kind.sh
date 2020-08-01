7744753f0bc77117fb0f289baf92d9be9603e690
Neal Hardesty <neal@crunchbase.com>
2020-08-01 08:50:44 -0700

    kind

---
+++ install-kubernetes-kind.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -ex
+
+
+sudo curl -Lo /usr/local/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.8.1/kind-linux-amd64 && sudo chmod a+x /usr/local/bin/kind
+
+
--- install-ubuntu-kubeadm.sh
+++ install-ubuntu-kubeadm.sh
@@ -13,3 +13,5 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
+
+sudo curl -Lo /usr/local/bin/kind https://github.com/kubernetes-sigs/kind/releases/download/v0.8.1/kind-linux-amd64 && sudo chmod a+x /usr/local/bin/kind
