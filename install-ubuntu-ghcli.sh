0cf3b1e29a65ee8feaa51b98df4e767673073e08
Neal Hardesty <neal@crunchbase.com>
2020-08-02 17:44:35 -0700

    ghcli

---
+++ install-ubuntu-ghcli.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+set -ex
+
+RELEASE=https://github.com/cli/cli/releases/download/v0.11.1/gh_0.11.1_linux_amd64.deb
+
+echo "Installing github CLI from ${RELEASE}"
+
+curl -Lso /var/tmp/gh.deb "${RELEASE}" 
+
+sudo dpkg -i /var/tmp/gh.deb
+
+rm /var/tmp/gh.deb
