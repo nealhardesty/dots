024f6410902dc6eeb314d4e880a72f6b5da04fec
Neal Hardesty <neal@crunchbase.com>
2020-08-10 09:01:45 -0700

    WIP

--- install-ubuntu-kubeadm.sh
+++ install-ubuntu-kubeadm.sh
@@ -4,7 +4,7 @@
 #sudo curl -o /usr/local/bin/kubectl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 #sudo chmod 755 /usr/local/bin/kubectl
 
-sudo apt-get update && apt-get install -y apt-transport-https curl
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
