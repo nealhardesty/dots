01fbb8478f669c72c5f64c7034decb30a7fe6ece
Neal Hardesty <neal@crunchbase.com>
2020-10-24 21:44:08 +0000

    updates

--- setup.sh
+++ setup.sh
@@ -2,24 +2,23 @@
 
 GITREPO=$(cd $(dirname $0); pwd)
 
-#find $GITREPO -maxdepth 1 -type f -name '.*' -exec echo {}  \;
-find $GITREPO -maxdepth 1 \! -name .gitignore -type f -name '.*' -exec cp -rv {} ~ \;
+find $GITREPO -maxdepth 1 \! -name .gitignore -type f -name '.*' -exec cp -rfv {} ~ \;
 
 mkdir -p ~/.config
-cp -rvs ${GITREPO}/.config/* ~/.config/
+cp -rvf ${GITREPO}/.config/* ~/.config/
 
-rm -rf ~/.vim
-cp -rv $GITREPO/.vim ~/.vim
+rm -rv ~/.vim
+cp -rfv $GITREPO/.vim ~/.vim
 #(mkdir -p $GITREPO/.vim/bundle && cd $GITREPO/.vim/bundle/ && rm -rf nerdtree && git clone https://github.com/scrooloose/nerdtree)
 
 rm -rf ~/.i3
-cp -rv $GITREPO/.i3 ~/.i3
+cp -rfv $GITREPO/.i3 ~/.i3
 
 touch ~/.bashrc.local
 
 if [ $(uname) == "Darwin" ]; then
   echo Must be a mac, installing hammerspoon config
-  cp -rv $GITREPO/.hammerspoon ~/.hammerspoon
+  cp -rfv $GITREPO/.hammerspoon ~/.hammerspoon
 fi
 
 
@@ -27,7 +26,7 @@ mkdir -p ~/.ssh
 if [ -f ~/.ssh/config ]; then
   echo Cowardly refusing to overwrite .ssh/config
 else
-  cp -rv $GITREPO/.ssh/config ~/.ssh/config
+  cp -rfv $GITREPO/.ssh/config ~/.ssh/config
   ls -1 ~/.ssh/id_rsa ~/.ssh/*.pem |xargs -I{} -n 1 echo IdentityFile={} | tee -a ~/.ssh/config.local
 fi
 
