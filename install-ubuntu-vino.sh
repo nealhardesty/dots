88d2ef596805a449f3ac28b4e7ec34a815ea73b6
Neal Hardesty <neal@crunchbase.com>
2020-11-11 10:11:25 -0800

    vino

--- install-ubuntu-vino.sh
+++ install-ubuntu-vino.sh
@@ -6,3 +6,6 @@ gsettings set org.gnome.Vino require-encryption false
 gsettings set org.gnome.Vino prompt-enabled false
 gsettings set org.gnome.Vino authentication-methods "['vnc']"
 gsettings set org.gnome.Vino vnc-password $(echo -n $(hostname) | base64)
+
+echo Start vino manually with:
+echo /usr/lib/vino/vino-server
