5961a52f58be45d6f209da59936b8c77a2d2f712
Neal Hardesty <neal@crunchbase.com>
2021-06-30 18:22:00 -0700

    gitignore

--- install-ubuntu-dev.sh
+++ install-ubuntu-dev.sh
@@ -51,6 +51,7 @@ mkdir -p ~/.ssh
 git config --global user.email "neal@crunchbase.com"
 git config --global user.name "Neal Hardesty"
 git config --global core.fileMode false
+git config pull.rebase true
 ssh-keyscan github.com > ~/.ssh/known_hosts
 
 # Docker time
