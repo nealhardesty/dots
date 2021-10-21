e488ae1c79979d8950c0dd68ed7a1df0cd56b3af
Neal Hardesty <neal@crunchbase.com>
2021-10-21 14:19:11 -0700

    xrdp fixes

--- install-ubuntu-xrdp.sh
+++ install-ubuntu-xrdp.sh
@@ -1,13 +1,40 @@
 #!/usr/bin/env bash
 
-sudo apt install xrdp xorgxrdp
+sudo apt install -y xrdp xorgxrdp
 
-cat <<MOOSE | sudo tee -a /etc/xrdp/xrdp.ini
-[xrdpvino]
-name=Vino-Session
-lib=libvnc.so
-username=${USER}
-password=ask
-ip=127.0.0.1
-port=5900
-MOOSE
+sudo ufw allow 3389
+
+
+# See https://c-nergy.be/blog/?p=15978
+
+#All Ubuntu version
+sudo bash -c "cat >/etc/polkit-1/localauthority/50-local.d/45-allow.colord.pkla" <<EOF
+[Allow Colord All Users]
+Identity=unix-user:*
+Action=org.freedesktop.color-manager.create-device;org.freedesktop.color-manager.create-profile;org.freedesktop.color-manager.delete-device;org.freedesktop.color-manager.delete-profile;org.freedesktop.color-manager.modify-device;org.freedesktop.color-manager.modify-profile
+ResultAny=no
+ResultInactive=no
+ResultActive=yes
+EOF
+
+#Specific Versions
+if [[ "$version" = *"Ubuntu 20.10"* ]] || [[ "$version" = *"Ubuntu 20.04"* ]] ;
+then
+  sudo bash -c "cat >/etc/polkit-1/localauthority/50-local.d/46-allow-update-repo.pkla" <<EOF
+[Allow Package Management All Users]
+Identity=unix-user:*
+Action=org.freedesktop.packagekit.system-sources-refresh
+ResultAny=yes
+ResultInactive=yes
+ResultActive=yes
+EOF
+fi
+
+# complains about access to /etc/xrdp/key.pem otherwise :shrug:
+sudo adduser xrdp ssl-cert
+
+sudo systemctl restart xrdp
+
+
+# Setup .xsession
+echo "/usr/bin/gnome-session" > ~/.xsession
