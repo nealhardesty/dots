c1270c13f6294188b822487931800c6e3c2e04d6
Neal Hardesty <neal@crunchbase.com>
2022-07-22 10:55:15 -0700

    containerd

--- install-ubuntu-containerd.sh
+++ install-ubuntu-containerd.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 
-#sudo curl -o /usr/local/bin/kubectl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-#sudo chmod 755 /usr/local/bin/kubectl
-
 . /etc/lsb-release
 [ ! -z "$DISTRIB_CODENAME" ] || exit 1
 echo Installing for DISTRIB_CODENAME=$DISTRIB_CODENAME
+
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
