10e5d7f8b737565a474b2d3d6e05702d7d5dbf7b
Neal Hardesty <neal@crunchbase.com>
2024-02-07 14:23:32 -0800

    wsl cuda test example

--- install-wsl2-cuda.sh
+++ install-wsl2-cuda.sh
@@ -7,3 +7,5 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt-get update
 sudo apt-get -y install cuda-toolkit-12-3
+
+sudo docker run --gpus all nvcr.io/nvidia/k8s/cuda-sample:nbody nbody -benchmark -gpu
