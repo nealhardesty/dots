c99de1843ce7302c6e1ef09fa0c49ab4518e9b0f
Neal Hardesty <neal@crunchbase.com>
2024-02-07 14:16:41 -0800

    wsl2 cuda

---
+++ install-wsl2-cuda.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=WSL-Ubuntu&target_version=2.0&target_type=runfile_local
+
+cd /var/tmp
+wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-12-3
