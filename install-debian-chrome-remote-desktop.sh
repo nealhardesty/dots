ba98ec6821292c448c694ee49db2afc9c3a093ae
Neal Hardesty <nealhardesty@yahoo.com>
2024-10-09 20:10:24 +0000

    code-server

---
+++ install-debian-chrome-remote-desktop.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+curl -o /tmp/chrome-remote-desktop_current_amd64.deb  https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt install -y /tmp/chrome-remote-desktop_current_amd64.deb
