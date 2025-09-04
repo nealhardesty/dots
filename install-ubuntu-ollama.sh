8abe1d9cb83bad0b284771555afdc8222baea48b
Neal Hardesty <nealhardesty@yahoo.com>
2025-09-04 15:02:45 -0700

    Updated ollama models

--- install-ubuntu-ollama.sh
+++ install-ubuntu-ollama.sh
@@ -5,13 +5,18 @@
 curl -fsSL https://ollama.com/install.sh | sudo sh
 
 echo https://ollama.com/library/llama3.1
-ollama pull llama3.1
+ollama pull llama3.1:8b
 # ollama run llama3.1:405b
-
 
 echo https://ollama.com/library/codellama
 ollama pull codellama
 # ollama pull codellama:34b
 
-echo https://ollama.com/library/gpt-oss
+echo https://ollama.com/library/gpt-oss:20b
 ollama pull gpt-oss
+
+echo https://ollama.com/library/deepseek-r1
+ollama pull deepseek-r1:8b
+
+echo https://ollama.com/library/qwen2.5-coder
+ollama pull qwen2.5-coder:7b
