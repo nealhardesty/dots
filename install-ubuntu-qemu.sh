35277016692bd0c558bfaa95dc23c6a162ffae79
Neal Hardesty <nealhardesty@yahoo.com>
2025-12-09 16:10:54 -0500

    Update QEMU installation script for Ubuntu: add 'virtiofsd' package to installation command for enhanced virtualization support.

--- install-ubuntu-qemu.sh
+++ install-ubuntu-qemu.sh
@@ -3,7 +3,7 @@
 set -ex
 
 sudo apt update
-sudo apt install -y qemu-kvm qemu-system-x86 libvirt-daemon-system libvirt-clients virt-manager dnsmasq ovmf swtpm
+sudo apt install -y qemu-kvm qemu-system-x86 libvirt-daemon-system libvirt-clients virt-manager dnsmasq ovmf swtpm sudo apt install virtiofsd
 
 # Enable and start libvirtd service
 sudo systemctl enable --now libvirtd
