a818898646968d61bd69ae7b96fab09b2567f178
Neal Hardesty <nealhardesty@yahoo.com>
2025-12-09 10:44:40 -0500

    Add installation script for QEMU on Ubuntu: automate package installation, enable libvirtd service, and configure user permissions for virtualization.

---
+++ install-ubuntu-qemu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+sudo apt update
+sudo apt install -y qemu-kvm qemu-system-x86 libvirt-daemon-system libvirt-clients virt-manager dnsmasq iptables-nft ovmf swtpm
+
+# Enable and start libvirtd service
+sudo systemctl enable --now libvirtd
+
+# Add current user to libvirt group
+sudo usermod -aG libvirt $USER
+
+# Enable default network
+sudo virsh net-autostart default
+sudo virsh net-start default || true
