eefc1b62844b5e767c736f295fd268c0f4aaaa5d
Neal Hardesty <nealhardesty@yahoo.com>
2026-01-06 12:43:06 -0500

    Update Ubuntu installation script: change shell to bash, add service configuration for Ollama, and restart service after installation.

--- install-ubuntu-ollama.sh
+++ install-ubuntu-ollama.sh
@@ -1,8 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+set -ex
 
 # https://ollama.com/download/linux
 
 curl -fsSL https://ollama.com/install.sh | sudo sh
+
+sudo mkdir -p /etc/systemd/system/ollama.service.d 
+printf "[Service]\nEnvironment=\"OLLAMA_HOST=0.0.0.0\"\n" | sudo tee /etc/systemd/system/ollama.service.d/override.conf 
+sudo systemctl daemon-reload 
+sudo systemctl restart ollama
 
 echo https://ollama.com/library/llama3.1
 ollama pull llama3.1:8b
